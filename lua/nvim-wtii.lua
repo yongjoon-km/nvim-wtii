daa3f25c0bc3b569885075a327e7d1da779126f6
jokingmoony <penubokim@gmail.com>
2024-02-07 22:40:29 +0900

    Open small window to show time

--- lua/nvim-wtii.lua
+++ lua/nvim-wtii.lua
@@ -1,11 +1,25 @@
 local M = {}
 local clock = require("nvim-wtii.clock")
 
-M.setup = function()
-end
+M.setup = function() end
 
 M.current_time = function()
-    print(clock.current_time())
+	local bufnr = vim.api.nvim_create_buf(false, true)
+	local lines = { clock.current_time() }
+
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+
+	local opts = {
+		style = "minimal",
+		relative = "editor",
+		width = 20,
+		height = 5,
+		row = 5,
+		col = 100,
+		border = "rounded",
+	}
+
+	local win_id = vim.api.nvim_open_win(bufnr, true, opts)
 end
 
 return M
