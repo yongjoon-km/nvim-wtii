ce35b15f31f65d95563903c6e3ad9dbf5f6dd9e6
jokingmoony <penubokim@gmail.com>
2024-02-17 00:32:03 +0900

    Refactor show_popup

--- lua/nvim-wtii/timer.lua
+++ lua/nvim-wtii/timer.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-local timer = nil
-local timer_window_id = nil
+local timer_obj = {
+    window_id = nil,
+    timer = nil
+}
 
 local get_sec = function(number, char)
 	if number == 0 then
@@ -41,7 +43,7 @@ local convert_to_sec = function(timer_str)
 	return sec
 end
 
-local show_popup = function(str)
+local show_popup = function(str, callback)
 	vim.schedule(function()
 		local bufnr = vim.api.nvim_create_buf(false, true)
 		local lines = { " " .. str .. " " }
@@ -65,83 +67,61 @@ local show_popup = function(str)
 			border = "rounded",
 		}
 
-		vim.api.nvim_open_win(bufnr, true, opts)
-	end)
-end
-
-local show_check_timer_popup = function(str)
-	vim.schedule(function()
-		local bufnr = vim.api.nvim_create_buf(false, true)
-		local lines = { " " .. str .. " " }
-
-		vim.api.nvim_buf_set_lines(bufnr, 1, -1, false, lines)
-
-		local width = string.len(str) + 2
-		local height = 3
-		local row = (vim.fn.winheight(0) / 2) - (height / 2)
-		local col = (vim.fn.winwidth(0) / 2) - (width / 2)
-
-		local opts = {
-			style = "minimal",
-			relative = "win",
-			width = width,
-			height = height,
-			row = row,
-			col = col,
-			border = "rounded",
-		}
-
-		timer_window_id = vim.api.nvim_open_win(bufnr, true, opts)
+		callback(vim.api.nvim_open_win(bufnr, true, opts))
 	end)
 end
 
 local update_popup = function(buf_str)
-	if not timer_window_id then
+	if not timer_obj.window_id then
 		return
 	end
 	local lines = { " " .. buf_str .. " sec" }
 	vim.schedule(function()
-		local bufnr = vim.fn.winbufnr(timer_window_id)
+		local bufnr = vim.fn.winbufnr(timer_obj.window_id)
         if bufnr == -1 then
             return
         end
+		vim.api.nvim_buf_set_option(bufnr, "modifiable", true)
+		vim.api.nvim_buf_set_option(bufnr, "readonly", false)
 		vim.api.nvim_buf_set_lines(bufnr, 1, -1, false, lines)
+		vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
+		vim.api.nvim_buf_set_option(bufnr, "readonly", true)
 	end)
 end
 
 local event_timer_update = function()
     local timer_update_loop = vim.uv.new_timer()
     timer_update_loop:start(100, 100, function()
-        if timer == nil or timer_window_id == nil then
+        if timer_obj.timer == nil or timer_obj.window_id == nil then
             return
         end
-        if vim.uv.timer_get_due_in(timer) == 0 then
+        if vim.uv.timer_get_due_in(timer_obj.timer) == 0 then
             return
         end
-        update_popup(math.floor(vim.uv.timer_get_due_in(timer) / 1000) .. " sec")
+        update_popup(math.floor(vim.uv.timer_get_due_in(timer_obj.timer) / 1000) .. " sec")
     end)
 end
 
 M.start_timer = function()
-    if timer ~= nil and vim.uv.timer_get_due_in(timer) ~= 0 then
+    if timer_obj.timer ~= nil and vim.uv.timer_get_due_in(timer_obj.timer) ~= 0 then
         print("timer is working currently")
         return
     end
 	vim.ui.input({ prompt = "timer value: (ex) 1h2m2s" }, function(input)
 		local timer_sec = convert_to_sec(input)
 
-		timer = vim.uv.new_timer()
-		timer:start(timer_sec * 1000, 0, function()
-			show_popup("time's up")
+		timer_obj.timer = vim.uv.new_timer()
+		timer_obj.timer:start(timer_sec * 1000, 0, function()
+			show_popup("time's up", function() end)
 		end)
 	end)
 end
 
 M.check_timer = function()
-    if not timer then
+    if not timer_obj.timer then
         return
     end
-    show_check_timer_popup(vim.uv.timer_get_due_in(timer) / 1000)
+    show_popup(vim.uv.timer_get_due_in(timer_obj.timer) / 1000, function(window_id) timer_obj.window_id = window_id end)
 end
 
 M.setup = function()
